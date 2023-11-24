7344a7d158360e34ad318d4038497381823764fd
aranibrian <aranibrian@gmail.com>
2023-11-24 13:30:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ session_start();
 
 <!-- Latest compiled JavaScript -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="js/script.js">
+<script src="js/script.js"></script>
 </head>
 <body>
     <div class="container">
